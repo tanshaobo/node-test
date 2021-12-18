0c157386dd49dc93a185b309a04e2a9790c4fac0
tanshaobo <18235140339@163.com>
2021-12-18 14:17:31 +0800

    create buffer

--- 06_buffer.js
+++ 06_buffer.js
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2021-12-15 14:17:17
  * @LastEditors: tanshaobo
- * @LastEditTime: 2021-12-17 15:13:06
+ * @LastEditTime: 2021-12-18 14:08:15
  * @Description: file content
  * @FilePath: \nodeTest\06_buffer.js
  */
@@ -22,4 +22,30 @@ console.log('UTF-8', buf.toString('UTF-8'))
 // latinl 一种把Buffer编码成一字编码的字符串的方式
 console.log('latin1', buf.toString('latin1'))
 
+/**
+ * 创建Buffer类 
+ */
 
+// 创建一个长度为10、且用0填充的Buffer
+const buf1 = Buffer.alloc(10)
+console.log('buf1',buf1)
+
+// 创建一个长度为10、且用0x1填充的Buffer
+const buf2=Buffer.alloc(10, 1)
+console.log('buf2', buf2)
+
+// 创建一个长度为10、且未初始化的Buffer，这个方法比alloc要快，但返回的Buffer实例可能包含旧数据，需要用fill或write重写
+const buf3 = Buffer.allocUnsafe(10)
+console.log('buf3', buf3)
+
+// 创建一个包含[0x1,0x2,0x3]的Buffer
+const buf4 = Buffer.from([1,2,3])
+console.log('buf4', buf4)
+
+// 创建一个包含 UTF-8 字节 [0x74, 0xc3, 0xa9, 0x73, 0x74] 的 Buffer
+const buf5 = Buffer.from('tést')
+console.log('buf5',buf5)
+
+// 创建一个包含Latin-1字节 [0x74, 0xc3, 0xa9, 0x73, 0x74] 的 Buffer
+const buf6 = Buffer.from('tést', 'latin1')
+console.log('buf6',buf6)
