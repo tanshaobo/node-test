bc62b87b93d25d1a615018010170a7962a9e0cbf
tanshaobo <18235140339@163.com>
2021-12-23 10:20:09 +0800

    添加Buffer copy

--- 06_buffer.js
+++ 06_buffer.js
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2021-12-15 14:17:17
  * @LastEditors: tanshaobo
- * @LastEditTime: 2021-12-22 11:19:35
+ * @LastEditTime: 2021-12-23 10:19:18
  * @Description: file content
  * @FilePath: \nodeTest\06_buffer.js
  */
@@ -119,3 +119,19 @@ const result2 = buf14.compare(buf13)
 
 console.log('result1',result1)
 console.log('result2',result2)
+
+/**
+ * Buffer 拷贝缓冲区
+ * 根据返回结果，类似full 从指定位置开始填充，填充到任一长度终止位置
+ */
+
+const buf15 = Buffer.from('abcd')
+
+const buf16 = Buffer.from('123')
+
+console.log('buf15',buf15)
+console.log('buf16',buf16)
+
+buf16.copy(buf15, 2)
+
+console.log(buf15.toString())
