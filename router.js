10fb8eb614c3d518f34d941a75f74fd9d586ccb3
tanshaobo <18235140339@163.com>
2021-12-27 17:23:24 +0800

    suppy info

--- router.js
+++ router.js
@@ -2,13 +2,13 @@
  * @Author: tanshaobo
  * @Date: 2021-12-24 17:10:22
  * @LastEditors: tanshaobo
- * @LastEditTime: 2021-12-24 17:49:16
+ * @LastEditTime: 2021-12-27 15:06:27
  * @Description: node router
  * @FilePath: \nodeTest\router.js
  */
 
 function route(pathname){
-  console.log('About to route a req')
+  console.log('About to route a request for ' + pathname)
 }
 
 exports.route = route
