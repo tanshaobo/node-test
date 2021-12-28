3f97b178b928af5cbddee4d31d60a89d29a1cce4
tanshaobo <18235140339@163.com>
2021-12-28 10:08:01 +0800

    node 全局对象

--- 11_global.js
+++ 11_global.js
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2021-12-27 17:24:28
  * @LastEditors: tanshaobo
- * @LastEditTime: 2021-12-27 17:31:45
+ * @LastEditTime: 2021-12-28 10:00:20
  * @Description: node 全局对象
  * @FilePath: \nodeTest\11_global.js
  */
@@ -19,3 +19,29 @@ setTimeout(() => {
 
 console.info('info')
 console.time()
+
+// 
+console.log('process', process)
+process.on('exit', (code) => {
+  setTimeout(() => {
+    console.log('该代码不会执行')
+  },0)
+  console.log(`退出码为${code}`)
+})
+console.log('程序执行结束')
+
+process.stdout.write('Hello World'+ '\n')
+
+console.log('process.config',process.config)
+// 当前平台
+console.log('process.platform',process.platform)
+// 当前node版本
+console.log('process.version',process.version)
+// 当前node版本和依赖
+console.log('process.versions',process.versions)
+// 当前进程的进程号
+console.log('process.pid', process.pid)
+// 当前进程的名称
+console.log('process.title',process.title)
+// 当前CPU架构
+console.log('process.arch', process.arch)
