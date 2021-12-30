299edb58cff122b87170f8f9616324e0d10dedb5
tanshaobo <18235140339@163.com>
2021-12-30 10:35:51 +0800

    添加截取文件

--- 13_fs.js
+++ 13_fs.js
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2021-12-29 11:24:04
  * @LastEditors: tanshaobo
- * @LastEditTime: 2021-12-29 15:51:57
+ * @LastEditTime: 2021-12-30 10:33:05
  * @Description: file
  * @FilePath: \nodeTest\13_fs.js
  */
@@ -27,7 +27,6 @@ fs.stat('input.txt', function(err, stats){
   if(err){
     return console.log(err)
   }
-  console.log(stats)
   console.log("读取文件信息成功！")
   console.log(`是否为文件（isFile）？ ${stats.isFile()}`)
   console.log(`是否为目录（isDirectory）？ ${stats.isDirectory()}`)
@@ -67,5 +66,74 @@ fs.open('input.txt', 'r+', function(err, fd){
     if(bytes > 0){
       console.log('bytes', buf.slice(0, bytes).toString())
     }
+  })
+})
+
+// 关闭文件
+console.log("close 准备打开文件！");
+fs.open('input.txt', 'r+', function(err, fd) {
+   if (err) {
+       return console.error(err);
+   }
+   console.log("close 文件打开成功！");
+   console.log("close 准备读取文件！");
+   fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+      if (err){
+         console.log(err);
+      }
+
+      // 仅输出读取的字节
+      if(bytes > 0){
+         console.log('close ', buf.slice(0, bytes).toString());
+      }
+
+      // 关闭文件
+      fs.close(fd, function(err){
+         if (err){
+            console.log(err);
+         } 
+         console.log("close 文件关闭成功");
+      });
+   });
+});
+
+// 截取文件
+console.log('ftruncate 准备打开文件')
+fs.open('input.txt', 'r+', function(err, fd){
+  if(err){
+    return console.error(err)
+  }
+
+  console.log('ftruncate 文件打开成功！')
+  console.log('ftruncate 截取10字节内的文件内容,超出部分将被去除！')
+
+  fs.ftruncate(fd, 10, function(err){
+    if(err){
+      console.log(err)
+    }
+
+    console.log("ftruncate 文件截取成功")
+    console.log("ftruncate 读取相同的文件")
+
+    fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+      if(err){
+        console.log(err)
+      }
+      
+      //仅输出读取的字节
+      if(bytes > 0){
+        console.log('ftruncate', buf.slice(0, bytes).toString())
+      }
+      
+      // 关闭文件
+      fs.close(fd, function(err){
+        if(err){
+          console.log(err)
+        }
+        
+        console.log("文件关闭成功！")
+      })
+    })
+
   })
 })
