a8c66155e2ebcf6dc28a6d9c5f3d57b11b0cc764
tanshaobo <18235140339@163.com>
2022-01-05 13:31:17 +0800

    模块系统移位

--- 06_model/model_01.js
+++ 06_model/model_01.js
@@ -2,14 +2,13 @@
  * @Author: tanshaobo
  * @Date: 2021-12-24 09:45:29
  * @LastEditors: tanshaobo
- * @LastEditTime: 2021-12-24 11:12:53
+ * @LastEditTime: 2022-01-05 13:29:50
  * @Description: 模块系统
- * @FilePath: \nodeTest\08_model.js
+ * @FilePath: \nodeTest\06_model\model_01.js
  */
 
-const Model = require('./model')
+const Model = require('./hello')
 
-console.log(Model)
 let model = new Model()
 
 model.setName('模块A')
