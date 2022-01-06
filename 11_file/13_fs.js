4161d03aa5c07e84a4b4494ad183931b5925411a
tanshaobo <18235140339@163.com>
2022-01-06 15:29:04 +0800

    拆分 读取文件数据

--- 11_file/13_fs.js
+++ 11_file/13_fs.js
@@ -2,33 +2,12 @@
  * @Author: tanshaobo
  * @Date: 2021-12-29 11:24:04
  * @LastEditors: tanshaobo
- * @LastEditTime: 2022-01-06 15:16:13
+ * @LastEditTime: 2022-01-06 15:28:40
  * @Description: file
  * @FilePath: \nodeTest\11_file\13_fs.js
  */
 
 const fs = require('fs')
-
-// 读取文件
-const buf = new Buffer.alloc(1024)
-console.log("准备打开已存在的文件！")
-fs.open('input.txt', 'r+', function(err, fd){
-  if(err){
-    return console.error(err)
-  }
-  console.log("文件打开成功！")
-  console.log("准备读取文件：")
-  fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
-    if(err){
-      console.log(err)
-    }
-    console.log(`${bytes}字节被读取`)
-
-    if(bytes > 0){
-      console.log('bytes', buf.slice(0, bytes).toString())
-    }
-  })
-})
 
 // 关闭文件
 console.log("close 准备打开文件！");
--- 11_file/readFileInfo.js
+++ 11_file/readFileInfo.js
@@ -2,7 +2,29 @@
  * @Author: tanshaobo
  * @Date: 2022-01-06 15:22:46
  * @LastEditors: tanshaobo
- * @LastEditTime: 2022-01-06 15:22:46
+ * @LastEditTime: 2022-01-06 15:27:49
  * @Description: 读取文件数据
  * @FilePath: \nodeTest\11_file\readFileInfo.js
  */
+const fs = require('fs')
+
+// 读取文件
+const buf = new Buffer.alloc(1024)
+console.log("准备打开已存在的文件！")
+fs.open('11_file/input.txt', 'r+', function(err, fd){
+  if(err){
+    return console.error(err)
+  }
+  console.log("文件打开成功！")
+  console.log("准备读取文件：")
+  fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+    if(err){
+      console.log(err)
+    }
+    console.log(`${bytes}字节被读取`)
+
+    if(bytes > 0){
+      console.log('bytes', buf.slice(0, bytes).toString())
+    }
+  })
+})
