a2bd3b2badf86c0f3e0d2e60bba75a19955c743e
tanshaobo <18235140339@163.com>
2022-01-17 11:29:07 +0800

    add 添加get请求

---
+++ 12_http/get.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: tanshaobo
+ * @Date: 2022-01-17 11:04:21
+ * @LastEditors: tanshaobo
+ * @LastEditTime: 2022-01-17 11:26:50
+ * @Description: get请求
+ * @FilePath: \nodeTest\12_http\get.js
+ */
+
+const http = require('http')
+const url = require('url')
+const util = require('util')
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'content-type':'text/plain; charset=utf-8' })
+  const param = url.parse(req.url, true).query
+  res.write(`角色: ${param.name}`)
+  res.write(`\n`)
+  res.write(`台词: ${param.url}`)
+  res.write(`\n`)
+  res.end(util.inspect(url.parse(req.url, true)))
+}).listen(3000)
