eb8cbb2483d5ec72ef8e38ceb3627e4f434821ea
tanshaobo <18235140339@163.com>
2022-01-18 11:25:27 +0800

    add express post请求

---
+++ 14_express/post.js
@@ -0,0 +1,40 @@
+/*
+ * @Author: tanshaobo
+ * @Date: 2022-01-18 11:01:28
+ * @LastEditors: tanshaobo
+ * @LastEditTime: 2022-01-18 11:23:20
+ * @Description: express post请求
+ * @FilePath: \nodeTest\14_express\post.js
+ */
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+// 创建application/x-www-form-urlencoded编码解析
+
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+
+app.use(express.static('public'))
+
+app.get('/index.html',(req, res) => {
+
+  res.sendFile(`${__dirname}/index_post.html`)
+})
+
+app.post('/process_post', urlencodedParser, (req, res)=> {
+  res.set({'Content-type': 'text/plain; charset=utf-8'})
+  // 输出 JSON 格式
+  const response = {
+    name: req.body.name,
+    code: req.body.code
+  }
+  console.log(response)
+  res.end(JSON.stringify(response))
+})
+
+const server = app.listen(9527, ()=> {
+  const host = server.address().address
+  const port = server.address().port
+  console.log('应用实例，访问地址为http://127.0.0.1:9527',host,port)
+})
