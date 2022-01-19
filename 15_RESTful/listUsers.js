d8a48c336df4ed4b54df13eb576f64f7601814b3
tanshaobo <18235140339@163.com>
2022-01-19 15:31:21 +0800

    add 接口 listUsers

--- 15_RESTful/listUsers.js
+++ 15_RESTful/listUsers.js
@@ -1,1 +1,22 @@
+/**
+ * RESTful  listUsers接口
+ */
 const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app.get('/listUsers',(req, res) => {
+  fs.readFile(`${__dirname}/users.json`, 'utf8', (err, data) => {
+    res.set({'Content-type': 'text/plain; charset=utf-8'})
+    console.log(err)
+    console.log(data)
+    res.end(data)
+  })
+})
+
+const server = app.listen(9527, () => {
+  const host = server.address().address
+  const port = server.address().port
+
+  console.log('应用实例，访问地址为：http://127.0.0.1:9527',host, port)
+})
